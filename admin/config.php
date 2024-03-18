bdfbf680fa1ff12126f48eb71c2c19a48aa4622c
John Doe <johndoe@example.com>
2024-03-19 00:20:16 +0300

    fix

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://onlinestore/admin/');
+define('HTTP_CATALOG', 'http://onlinestore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://onlinestore/admin/');
+define('HTTPS_CATALOG', 'http://onlinestore/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/onlinestore/admin/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/onlinestore/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/onlinestore/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/onlinestore/admin/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/onlinestore/admin/view/template/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/onlinestore/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/onlinestore/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/onlinestore/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/onlinestore/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/onlinestore/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/onlinestore/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OSPanel/domains/onlinestore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'online_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://onlinestore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://onlinestore/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/onlinestore/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/onlinestore/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/onlinestore/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/onlinestore/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/onlinestore/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/onlinestore/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/onlinestore/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/onlinestore/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/onlinestore/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/onlinestore/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/onlinestore/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'online_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
